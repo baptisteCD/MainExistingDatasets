6739803cf1d7f5f1e93dd1188615563908cef9f6
ecamenen <etienne.camenen@gmail.com>
2022-04-12 18:23:44 +0200

    Add the number of datasets per country on the map

--- R/app_server.R
+++ R/app_server.R
@@ -12,24 +12,12 @@ app_server <- function(input, output, session) {
         data = read.xlsx(file.path(path, file))
     )
 
-    human_dataset_countries <- reactive({
-        countries_db <- unique(trimws(unlist(strsplit(vars$data$Country, "[&/]"))))
-        countries_db <- gsub("-", " ", countries_db[!grepl("wide", countries_db)])
-
-        countries_db[countries_db == "UK"] <- "United Kingdom"
-        countries_db[countries_db == "Sout Korea"] <- "Republic of Korea"
-        countries_db[countries_db == "Barcelona"] <- "Spain"
-
-        countries <- intersect(countries_db, world$name_long)
-
-        world[world$name_long %in% countries, "name_long"]
-    })
-
     output$map <- renderTmap({
+        human_dataset <- get_table_countries(vars$data)
         tm_shape(world, bbox = bb(matrix(c(50, 75, -20, -50), 2, 2))) +
             tm_borders(col = "gray", alpha = 0.5) +
-            tm_shape(human_dataset_countries()) +
-            tm_fill(col = "red", alpha = 0.3, id = "name_long") +
+            tm_shape(human_dataset) +
+            tm_fill(col = "N", id = "name_long", style = "cat") +
             tm_layout(title = "Datasets around the world")
     })
 
