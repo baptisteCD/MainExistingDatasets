9444e055baf8978d3a84e39d8c75ee58f9062e3e
ecamenen <etienne.camenen@gmail.com>
2022-05-16 18:10:42 +0200

    Update config for tidyr

--- R/MainExistingDatasets-package.R
+++ R/MainExistingDatasets-package.R
@@ -5,8 +5,9 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
-#' @import dplyr DT golem glue htmltools htmlwidgets magrittr openxlsx pkgload
-#' @import processx sf spData tmap tmaptools utils
+#' @import dplyr DT golem glue htmltools htmlwidgets openxlsx pkgload
+#' @import processx sf spData tidyr tmap tmaptools utils
+#' @rawNamespace import(magrittr, except = extract)
 #' @rawNamespace import(rlang, except = c(ns_env, pkg_env, set_names))
 #' @rawNamespace import(shiny, except = c(dataTableOutput, renderDataTable))
 NULL
