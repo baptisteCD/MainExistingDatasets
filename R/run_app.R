71b3c994263e9517808f4f01cdbff772df588cd5
ecamenen <etienne.camenen@gmail.com>
2022-06-16 17:02:59 +0200

    Complete the documentation of run_app

--- R/run_app.R
+++ R/run_app.R
@@ -2,8 +2,19 @@
 #'
 #' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
+#' @return An object that represents the app.
 #' @inheritParams shiny::shinyApp
+#' @inherit shiny::runApp description
+#' @details For more information about this function, please take a look at 
+#' \url{https://cran.r-project.org/web/packages/golem/vignettes/c_deploy.html}.
+#' @examples
+#' \dontrun{
+#' # Start app in the current working directory
+#' run_app()
 #'
+#' # Start app in a subdirectory called myapp
+#' run_app("myapp")
+#' }
 #' @export
 run_app <- function(
     onStart = NULL,
